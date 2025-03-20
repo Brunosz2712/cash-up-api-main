b5e50a95a65c6346bfc7697266c2b6681a3f0707
Brunosz2712 <brunosouza_27@yahoo.com.br>
2025-03-20 09:48:56 -0300

    Adicionando db

--- cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/model/Category.java
+++ cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/model/Category.java
@@ -1,37 +1,36 @@
 package br.com.fiap.cash_up_api.model;
-
-import java.util.Random;
-
+ 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+ 
+@Entity //comunica com o jpa
+ 
 public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //Id seja a chave primária || O banco de dados que gera o id
     private Long id;
     private String name;
     private String icon;
-
-    public Category(Long id, String name, String icon) {
-        this.id = Math.abs(new Random().nextLong());
-        this.name = name;
-        this.icon = icon;
-    }
-
+ 
     public Long getId() {
         return id;
     }
-
+ 
     public String getName() {
         return name;
     }
-
+ 
     public String getIcon() {
         return icon;
     }
-
-    public void setId(Long id) {
+ 
+    public void setId(Long id) { //PARA NÃO ALTERAR O ID QUANDO UMA CATEGORIA FOR ALTERADA
         this.id = id;
     }
-
-    @Override
+ 
+    @Override // TODA VEZ QUE CHAMAR UMA CATEGORIA ELE VAI APARECER DESSA FORMA
     public String toString() {
-        return id + "-" + name + "-" + icon;
+        return id + " - " + name + " - " + icon;
     }
-
 }
